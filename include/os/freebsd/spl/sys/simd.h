acc3d248130c9c23dd7311447f59e4cd40e3093b
Tino Reichardt <milky-zfs@mcmilk.de>
2024-01-16 22:43:49 +0100

    SPL kfpu enter/leave

--- include/os/freebsd/spl/sys/simd.h
+++ include/os/freebsd/spl/sys/simd.h
@@ -43,14 +43,11 @@
 
 #else
 #define	kfpu_allowed()		0
-#define	kfpu_initialize(tsk)	do {} while (0)
 #define	kfpu_begin()		do {} while (0)
 #define	kfpu_end()		do {} while (0)
-#define	kfpu_init()		(0)
-#define	kfpu_fini()		do {} while (0)
 #endif
 
-#define	simd_stat_init()	0
-#define	simd_stat_fini()	0
+#define	simd_stat_init()
+#define	simd_stat_fini()
 
 #endif
