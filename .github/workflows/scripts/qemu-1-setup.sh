bd6f8f13b3105710d95d1c7c00cac31be9c72dd6
Tino Reichardt <milky-zfs@mcmilk.de>
2024-07-25 00:04:06 +0200

    ppc

--- .github/workflows/scripts/qemu-1-setup.sh
+++ .github/workflows/scripts/qemu-1-setup.sh
@@ -6,17 +6,11 @@
 
 #set -eu
 
-# Filesystem      Size  Used Avail Use% Mounted on
-# /dev/root        72G   42G   31G  58% /     -> 25GB
-# /dev/sdb1        74G  4.1G   66G   6% /mnt  -> 60GB
-
 # speedup virtual disk
-sudo modprobe loop max_loop=2
+sudo modprobe loop max_loop=1
 sudo fallocate -l 50GB /var/blob
-sudo fallocate -l 90GB /mnt/blob
 sudo losetup -f /var/blob
-sudo losetup -f /mnt/blob
-sudo mdadm --create /dev/md/raidzero -n2 -l0 /dev/loop{0..1}
+sudo mdadm --create /dev/md/raidzero -n2 -l0 /dev/loop0 /dev/sdb
 sudo mkfs.ext4 -F /dev/md/raidzero
 
 # old /mnt is hidden now
@@ -25,6 +19,7 @@ sudo mount -o discard,noatime /dev/md/raidzero /mnt/tests
 df -h / /mnt /mnt/tests
 
 # install needed packages
+  export DEBIAN_FRONTEND="noninteractive"
 sudo apt-get update
 sudo apt-get install axel cloud-image-utils daemonize guestfs-tools \
   virt-manager linux-modules-extra-`uname -r`
