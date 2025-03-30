040cbbbce9a5890a3cb21dbe8e198104f6b55360
Tino Reichardt <milky-zfs@mcmilk.de>
2025-03-30 08:21:45 +0200

    bsd

--- .github/workflows/scripts/qemu-2-start.sh
+++ .github/workflows/scripts/qemu-2-start.sh
@@ -73,7 +73,6 @@ case "$OS" in
     OSNAME="FreeBSD 13.4-RELEASE"
     OSv="freebsd13.0"
     URLzs="$FREEBSD/amd64-freebsd-13.4-RELEASE.qcow2.zst"
-    URLzs="https://download.freebsd.org/releases/VM-IMAGES/14.2-RELEASE/amd64/Latest/FreeBSD-14.2-RELEASE-amd64-BASIC-CLOUDINIT.ufs.qcow2.xz"
     BASH="/usr/local/bin/bash"
     NIC="rtl8139"
     ;;
@@ -81,14 +80,14 @@ case "$OS" in
     OSNAME="FreeBSD 14.2-RELEASE"
     OSv="freebsd14.0"
     URLzs="$FREEBSD/amd64-freebsd-14.2-RELEASE.qcow2.zst"
-    URLzs="https://download.freebsd.org/releases/VM-IMAGES/14.2-RELEASE/amd64/Latest/FreeBSD-14.2-RELEASE-amd64-BASIC-CLOUDINIT.ufs.qcow2.xz"
+    URLxz="https://download.freebsd.org/releases/VM-IMAGES/14.2-RELEASE/amd64/Latest/FreeBSD-14.2-RELEASE-amd64-BASIC-CLOUDINIT.ufs.qcow2.xz"
     BASH="/usr/local/bin/bash"
     ;;
   freebsd13-5r)
     OSNAME="FreeBSD 13.4-STABLE"
     OSv="freebsd13.0"
     URLzs="$FREEBSD/amd64-freebsd-13.4-STABLE.qcow2.zst"
-    URLzs="https://download.freebsd.org/releases/VM-IMAGES/13.5-RELEASE/amd64/Latest/FreeBSD-13.5-RELEASE-amd64.qcow2.xz"
+    URLxz="https://download.freebsd.org/releases/VM-IMAGES/13.5-RELEASE/amd64/Latest/FreeBSD-13.5-RELEASE-amd64.qcow2.xz"
     BASH="/usr/local/bin/bash"
     NIC="rtl8139"
     ;;
@@ -102,7 +101,7 @@ case "$OS" in
     OSNAME="FreeBSD 15.0-CURRENT"
     OSv="freebsd14.0"
     URLzs="$FREEBSD/amd64-freebsd-15.0-CURRENT.qcow2.zst"
-    URLzs="https://download.freebsd.org/releases/VM-IMAGES/14.2-RELEASE/amd64/Latest/FreeBSD-14.2-RELEASE-amd64-BASIC-CLOUDINIT.ufs.qcow2.xz"
+    URLxz="https://download.freebsd.org/releases/VM-IMAGES/14.2-RELEASE/amd64/Latest/FreeBSD-14.2-RELEASE-amd64-BASIC-CLOUDINIT.ufs.qcow2.xz"
     BASH="/usr/local/bin/bash"
     ;;
   tumbleweed)
